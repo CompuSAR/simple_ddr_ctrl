3a516955649a91bc015e7c8ec5cbd9884ab36495
Shachar Shemesh <shachar@shemesh.biz>
2023-01-08 18:08:18 +0200

    Physical interfaces

--- sddr_phy_xilinx.sv
+++ sddr_phy_xilinx.sv
@@ -1,6 +1,10 @@
 `timescale 1ns / 1ps
 
 module sddr_phy_xilinx#(
+        BANK_BITS = 3,
+        ROW_BITS = 13,
+        COL_BITS = 10,
+        DATA_BITS = 16
     )
     (
         // Inside interfaces
@@ -10,12 +14,28 @@ module sddr_phy_xilinx#(
 
 
         // Outside interfaces
-        output ddr3_ck_p_o,
-        output ddr3_ck_n_o,
-        output ddr3_reset_n_o
+        output                                          ddr3_ck_p_o,
+        output                                          ddr3_ck_n_o,
+        output                                          ddr3_reset_n_o,
+
+        output                                          ddr3_cke_o,
+        output                                          ddr3_ras_n_o,
+        output                                          ddr3_cas_n_o,
+        output                                          ddr3_we_n_o,
+
+        output                                          ddr3_cs_n_o,
+
+        output [BANK_BITS-1:0]                          ddr3_ba_o,
+        output [ROW_BITS+$clog2(DATA_BITS/8)-1:0]       ddr3_addr_o,
+        output                                          ddr3_odt_o,
+        output [$clog2(DATA_BITS/8):0]                  ddr3_dm_o,
+        inout [$clog2(DATA_BITS/8):0]                   ddr3_dqs_p_io,
+        inout [$clog2(DATA_BITS/8):0]                   ddr3_dqs_n_io,
+        inout [DATA_BITS-1:0]                           ddr3_dq_io
     );
 
 assign ddr3_reset_n_o = !in_ddr_reset_p_i;
+assign ddr3_cs_n_o = 1'b0;      // We don't do chip select
 
 // Clock differential output
 reg naked_clock_signal;
